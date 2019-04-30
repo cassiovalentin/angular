1d727ec29c60afdd976a7c91242d76793396e220
cassiovalentin <cassiovrsilva@gmail.com>
2019-04-30 16:57:19 -0300

    barra-de-navegacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelSimplesComponent
+    PainelSimplesComponent,
+    BarraNavegacaoComponent
    ],
   imports: [
     BrowserModule,
