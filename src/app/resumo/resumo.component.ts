436032585bf9d77072ede0f7ad4ce0353e8f3506
cassiovalentin <cassiovrsilva@gmail.com>
2019-05-08 07:26:11 -0300

    service

--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ResumoService } from './resumo.service';
 @Component({
   selector: 'app-resumo',
   templateUrl: './resumo.component.html',
@@ -8,31 +9,12 @@ import { HttpClient } from '@angular/common/http';
 export class ResumoComponent implements OnInit {
 
   resumo ;
-  /*= {
-    consultas : {
-      consultas_30dias_anteriores : 87,
-      consultas_30dias_posteriores : 79
-
-    },
-    faturamento : {
-      anterior : {
-        valor : 100000,
-        comparativo : 19,
-
-      },
-
-      previsao : {
-        valor : 90000,
-        comparativo : -10
-      }
-    }
-  };
-  */
-  constructor(private http: HttpClient) {   }
+ 
+  constructor(private resumoService: ResumoService) {   }
 
   ngOnInit() {
-    this.http.get('http://www.devup.com.br/php/api-dashboard/api/resumo')
-    .subscribe(dados => this.resumo = dados);
+   this.resumoService.getResumo()
+   .subscribe(dados => this.resumo = dados);
 
   }
 
